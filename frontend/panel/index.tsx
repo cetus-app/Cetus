5cd4270d3d9149243060414394a2eb4a7f053b05
Josh Muir <josh@muir.xyz>
2020-04-29 22:52:24 +0100

    setup frontend

---
+++ frontend/panel/index.tsx
@@ -0,0 +1,11 @@
+// The entry point for the panel React application.
+// index.tsx
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './panel.css'
+import 'bulma/bulma.sass'
+
+ReactDOM.render(
+  <p className="has-text-centered">Hello</p>,
+  document.getElementById('root'),
+);
