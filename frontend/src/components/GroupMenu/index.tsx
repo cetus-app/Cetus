229db26cf7719f908a8c2d98e4f554025676e6b1
Josh Muir <josh@muir.xyz>
2020-05-02 17:49:05 +0100

    add crlf rule to eslint

---
+++ frontend/src/components/GroupMenu/index.tsx
@@ -0,0 +1,14 @@
+// Created by josh on 01/05/2020
+// This handles the rendering of a collection of groups. It fires the given event with the group ID.
+// It is used to both render "Linked groups" and "unlinked groups"
+import React, {FunctionComponent} from "react";
+
+
+interface GroupMenuProps {
+
+}
+
+export const GroupMenu: FunctionComponent<GroupMenuProps> = (props) => {
+  return <div></div>
+};
+export default GroupMenu
