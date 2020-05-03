ac30f1fcfcb3bd89dd477d85cb0e8b292bb69cfe
Josh Muir <josh@muir.xyz>
2020-05-03 20:12:54 +0100

    Start groupcontroller

---
+++ backend/controllers/GroupController.ts
@@ -0,0 +1,29 @@
+import { IsBoolean } from "class-validator";
+import {
+  Authorized, CurrentUser, Get, JsonController
+} from "routing-controllers";
+import { ResponseSchema } from "routing-controllers-openapi";
+
+import Group from "../entities/Group.entity";
+import User from "../entities/User.entity";
+
+
+class GetGroupsResonse {
+  @IsBoolean()
+  success: boolean;
+
+  // TODO: Make this a data transfer object
+  // Add validation?
+  groups: Group[];
+}
+
+@JsonController("/groups")
+export default class GroupController {
+  @Get("/")
+  @ResponseSchema(GetGroupsResonse)
+  @Authorized()
+  async getGroups (@CurrentUser({ required: true }) user: User): Promise<GetGroupsResonse> {
+    // Get user groups
+    // Needs the database service.. RIP.
+  }
+}
