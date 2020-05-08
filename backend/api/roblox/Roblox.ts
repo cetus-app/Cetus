11a405ee82daa097c9cccad8b091039ab20d818b
Eirik Fossestøl Andresen <eiriand03@gmail.com>
2020-05-08 21:34:32 +0200

    Add method for getting blurb

--- backend/api/roblox/Roblox.ts
+++ backend/api/roblox/Roblox.ts
@@ -4,6 +4,7 @@ import { ExternalHttpError } from "../../shared";
 import checkStatus from "../../shared/util/fetchCheckStatus";
 
 export const BASE_API_URL = "https://api.roblox.com";
+export const USERS_API_URL = "https://users.roblox.com";
 
 export default class Roblox {
   static async getIdFromUsername (username: string): Promise<number | undefined> {
@@ -15,5 +16,14 @@ export default class Roblox {
     if (data.success === false && data.errorMessage && data.errorMessage.toLowerCase() === "user not found") return undefined;
 
     throw new ExternalHttpError(url, "Error while getting ID from username");
+  }
+
+  static async getBlurb (id: number): Promise<string> {
+    const url = `${USERS_API_URL}/v1/users/${id}`;
+    const data = await fetch(url).then(checkStatus).then(res => res && res.json());
+
+    if (!data) throw new Error("Invalid ID passed to `getBlurb`");
+
+    return data.description;
   }
 }
