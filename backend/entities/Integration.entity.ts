a0eaf74c34407267ab8ea9c3a11b0ac75a964726
Eirik Fossestøl Andresen <eiriand03@gmail.com>
2020-06-04 15:51:19 +0200

    Add integration type

--- backend/entities/Integration.entity.ts
+++ backend/entities/Integration.entity.ts
@@ -2,6 +2,7 @@
 // may want to look at JSON fields or something as it's likely the shape of the data will vary by integration.
 // An alternative would be to have a different entity for each integration, I guess?
 import {
+  Column,
   Entity,
   ManyToOne,
   PrimaryGeneratedColumn
@@ -9,11 +10,24 @@ import {
 
 import Group from "./Group.entity";
 
+export enum IntegrationType {
+  promotionCentre = "PROMOTION_CENTRE",
+  discordBot = "DISCORD_BOT",
+  rankingAPI = "RANKING_API",
+  antiAdminAbuse = "ANTI_ADMIN_ABUSE"
+}
+
 @Entity()
 export default class Integration {
   // UUID V4
   @PrimaryGeneratedColumn("uuid")
   id: string;
+
+  @Column({
+    type: "enum",
+    enum: IntegrationType
+  })
+  type: IntegrationType;
 
   @ManyToOne(() => Group, grp => grp.integrations, { nullable: false })
   group: Group;
