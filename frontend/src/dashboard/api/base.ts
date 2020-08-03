86f70f713bf9a6b78479136e5da0fd19eef5eadb
Neztore <josh@muir.xyz>
2020-08-03 22:18:24 +0100

    Add CSRF protection (#19)

--- frontend/src/dashboard/api/base.ts
+++ frontend/src/dashboard/api/base.ts
@@ -1,17 +1,19 @@
 import { checkStatus } from ".";
+import { getCookie } from "../components/shared";
 
 export interface RequestOptions extends Omit<RequestInit, "body"> {
   body?: {
     [key: string]: any;
   }
 }
-
 const baseFetch = (input: RequestInfo, options?: RequestOptions | undefined): Promise<Response> => {
   const body = options?.body ? JSON.stringify(options.body) : undefined;
+  const csrfToken = getCookie("CSRF-Token");
 
   const headers = {
     Accept: "application/json",
     "Content-Type": "application/json",
+    "CSRF-Token": csrfToken,
     ...options?.headers
   };
 
--- frontend/src/dashboard/components/shared/utils.ts
+++ frontend/src/dashboard/components/shared/utils.ts
@@ -9,4 +9,20 @@ export const PasswordValidation = Yup.string()
   .min(6, "A password must be at least 6 characters long.")
   .max(100, "Must be 100 characters or less")
   .required("This field is required.");
+export function getCookie (cname: string) {
+  const name = `${cname}=`;
+  const decodedCookie = decodeURIComponent(document.cookie);
+  const ca = decodedCookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) === " ") {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
 export default typedKeys;
