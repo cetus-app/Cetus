2ce5b69faefedbd731ad6ca561c28800bf3e3846
Eirik Fossestøl Andresen <eiriand03@gmail.com>
2020-08-06 20:16:14 +0200

    Stripe temporary ID update

--- backend/constants/index.ts
+++ backend/constants/index.ts
@@ -6,6 +6,6 @@ export const hashRounds = 12;
 export const authLife = 1209600000;
 export const cetusGroupId = 6245155;
 
-export const stripeGroupPriceId = "price_1Gxbg7GqpYObsvKxxgtbaRb5";
+export const stripeGroupPriceId = "price_1HDD2oGqpYObsvKxZ8LLqzyd";
 
 export const botGroupThreshold = 95;
