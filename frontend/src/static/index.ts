3b3e77a6bcf5aea1b41ac259027b6c09d5223d25
Josh Muir <josh@muir.xyz>
2020-08-07 01:52:21 +0100

    Improve terms, add class "discord-invite" which is autoset

--- frontend/src/static/index.ts
+++ frontend/src/static/index.ts
@@ -1,2 +1,10 @@
 // HMR does not work for the static pages without a loaded JS/TS file
-console.log("Testing empty file");
+const links = document.getElementsByClassName("discord-invite") as HTMLCollectionOf<HTMLAnchorElement>;
+for (let i = 0; i < links.length; i++) {
+  const item = links.item(i);
+  if (item) {
+    item.href = process.env.discordInvite || "#";
+    item.target = "_blank";
+    item.rel = "noopener noreferrer";
+  }
+}
