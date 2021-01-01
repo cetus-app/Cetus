529762d811fcdb629714c87198f9d5eba313bf70
Eirik <eiriand03@gmail.com>
2021-01-01 17:51:48 +0100

    Revert "Billing portal (#31)"
    This reverts commit 5dfa956f2de6609083344579f1229df8c1a7bbf2.

--- backend/controllers/PaymentController/types.ts
+++ backend/controllers/PaymentController/types.ts
@@ -23,13 +23,7 @@ export class SessionResponse {
   sessionId: string;
 }
 
-export class CustomerPortalSessionResponse {
-  @IsString()
-  @IsNotEmpty()
-  url: string;
-}
-
-export class StripeWebhookResponse {
+export class CompleteSubscriptionResponse {
   // Stripe webhook received acknowledgement (only a 2xx response is actually required)
   received: boolean;
 }
